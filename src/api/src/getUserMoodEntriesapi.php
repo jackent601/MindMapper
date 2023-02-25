468e93d847b3c04d988780634c2551bf9c3d8da0
Jack <jackent601@gmail.com>
2023-02-25 14:13:52 +0000

    integrates with verifyapi to automate getting user_id

--- src/api/src/getUserMoodEntriesapi.php
+++ src/api/src/getUserMoodEntriesapi.php
@@ -1,18 +1,5 @@
 <?php
-
-
-// foreach (getallheaders() as $name => $value) {
-//     echo nl2br("$name: $value\n");
-// }
-// $headers = getallheaders();
-// echo nl2br("\n");
-// echo nl2br("\n");
-// echo $headers["Accept-Encoding"];
-
-// http_response_code(200);
-// echo $response;
-
-
+    session_start();
 ?>
 
 <?php
@@ -20,62 +7,32 @@
 
     if ($_SERVER['REQUEST_METHOD']==='GET') {
 
-        // Check api key provided
-        if (!in_array("X-API-KEY", array_keys(getallheaders()))){
-            http_response_code(404);
-            echo json_encode(["message" => "No API key provided","headers_provided" => getallheaders()]);
-            exit;
-        }
+        // Verify API key (only proceeds if valid key), API key included automatically in request header through session
+        // sets the $user_id variable if key is valid
+        include "verifyApiKeyHeader.php";
 
-        $requestHeaders = getallheaders();
-        $api_key = $requestHeaders["X-API-KEY"];
-        // if (!$api_key){
-        //     http_response_code(404);
-        //     echo json_encode(["message" => "No API key provided"]);
-        //     exit;
-        // } 
-        
-        // Check api key (this will be used a lot, maybe functionalise?)
-        include "dbconn.php";
-        $apiCheckSQL = "SELECT * FROM api_keys WHERE api_key = '$api_key'";
-        $apiCheck = $conn->query($apiCheckSQL);
-
-        if (!$apiCheck) {
+        // Read User Mood Entries
+        $moodQuery = "SELECT * FROM mood_entry WHERE user_id = '$user_id'";
+        $moodEntries = $conn->query($moodQuery);
+        if (!$moodEntries) {
             exit($conn->error);
-        }
+        } 
         
-        if ($apiCheck->num_rows > 0) {
+        // build a response array
+        $api_response = array();
+        foreach ($moodEntries as $row) {
+            array_push($api_response, $row);
+        }
+
+        // encode the response as JSON
+        $response = json_encode($api_response);
+
+        // Send results
+        if ($response != false) {
             http_response_code(200);
-            echo json_encode(["message" => "Valid API key provided"]);
-        }
-        
-        else {
+            echo $response;
+        } else {
             http_response_code(404);
-            echo json_encode(["message" => "Invalid API key provided"]);
+            echo json_encode(["message" => "Failed querying Database"]);
         }
-        
-        // include "dbconn.php";
-        // $readSQL = "SELECT * FROM runschedule";
-        // $result = $conn->query($readSQL);
-
-        // if (!$result) {
-        //     exit($conn->error);
-        // }
-
-        // // build a response array
-        // $api_response = array();
-        // while ($row = $result->fetch_assoc()) {
-        //     array_push($api_response, $row);
-        // }
-        // // encode the response as JSON
-        // $response = json_encode($api_response);
-
-        // // echo out the response
-        // if ($response != false) {
-        //     http_response_code(200);
-        //     echo $response;
-        // } else {
-        //     http_response_code(404);
-        //     echo json_encode(["message" => "Unable to process response!"]);
-        // }
     }
