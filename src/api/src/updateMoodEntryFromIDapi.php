ec6e4cadccff31727ac96ac0bbca5ecdfa3d832b
Jack <jackent601@gmail.com>
2023-02-26 21:39:30 +0000

    edit mood feature added

---
+++ src/api/src/updateMoodEntryFromIDapi.php
@@ -0,0 +1,69 @@
+<?php
+    session_start();
+?>
+<?php
+    /*
+    updateMoodEntryFromIDapi API Documentation:
+
+        Updates a single mood entry based on the mood id provided
+
+    Return:
+        bool: succesful delete or not
+        redirects to homepage
+
+    */
+    header("Content-Type: application/json");
+
+    if ($_SERVER['REQUEST_METHOD']==='POST') {
+        if (isset($_POST['MOOD_ENTRY_ID'])){
+            if (isset($_POST['MOOD_CONTEXT_UPDATE'])){
+                $mood_entry_id = $_POST['MOOD_ENTRY_ID'];
+                $new_mood_context = $_POST['MOOD_CONTEXT_UPDATE'];
+            
+                // Verify API key (see documentation)
+                include "verifyApiKeyHeader.php";
+    
+                // Check Mood ID is valid (could be functionalised)
+                $checkIDQuery = "SELECT id FROM mood_entry WHERE id='$mood_entry_id';";
+                $idCheck = $conn->query($checkIDQuery);
+                // Check Not Empty
+                if($idCheck->num_rows <= 0){
+                    http_response_code(404);
+                    echo json_encode(["message" => "No rows for entry, likely invalid mood entry id"]);
+                    exit;
+                }
+                // Check Database integrity
+                if($idCheck->num_rows > 1){
+                    http_response_code(404);
+                    echo json_encode(["message" => "Multiple rows for ID, database integrity compromised"]);
+                    exit;
+                }
+    
+                // update mood entry
+                $updateEntryQuery = "UPDATE mood_entry SET context='$new_mood_context' WHERE id='$mood_entry_id';";
+                $updated = $conn->query($updateEntryQuery);
+    
+                if ($updated){
+                    // Successful deletion, redirect to homepage
+                    http_response_code(200);
+                    header("location: http://localhost/project/src/client/index.php");
+                    exit;
+                }else{
+                    http_response_code(404);
+                    echo json_encode(["message" => "Error in database connection"]);
+                    exit($conn->error);
+                }
+
+            }else{
+                http_response_code(404);
+                echo json_encode(["message" => "No Mood Context Update provided"]);
+            }
+        }else{
+            http_response_code(404);
+            echo json_encode(["message" => "No Mood Entry id provided"]);
+        }
+    }else{
+        http_response_code(404);
+        echo json_encode(["message" => "Unsupported request method"]);
+        exit;
+    }
--- src/client/js/displayEditMoodEntryForm.js
+++ src/client/js/displayEditMoodEntryForm.js
@@ -24,32 +24,43 @@ function confirmDelete(e, api_key, mood_entry_id){//}, api_key, mood_entry_id){
     }
 }
 
-// function confirmEdit(e, api_key, mood_entry_id){//}, api_key, mood_entry_id){
-//     if(!confirm('Update this entry?')) {
-//         e.preventDefault();
-//     }else{
-//         // prevent default for manual request and redirect
-//         e.preventDefault();
-//         // Send Delete Query
-//         var xmlHttp = new XMLHttpRequest();
-//         xmlHttp.open("POST", "http://localhost/Project/src/api/src/updateMoodEntryFromIDapi.php", false); // false for synchronous request
-//         // API key
-//         xmlHttp.setRequestHeader("X-API-KEY", api_key)
+function confirmEdit(e, api_key, mood_entry_id){//}, api_key, mood_entry_id){
+    if(!confirm('Update this entry?')) {
+        e.preventDefault();
+    }else{
+        // prevent default for manual request and redirect
+        e.preventDefault();
 
-//         // Form Data
-//         var data;
-//         data = new FormData();
-//         data.append('MOOD_ENTRY_ID', mood_entry_id);
-//         data.append('MOOD_CONTEXT', mood_entry_id);
+        // Get new mood context
+        var formDataSerial = $('#moodEditForm').serializeArray();
+        var formData = {};
+        for (var i = 0; i < formDataSerial.length; i++){
+            formData[formDataSerial[i]['name']] = formDataSerial[i]['value'];
+        }
+        var newMoodContext = formData['moodContext'];
+        console.log("New mood context: "+newMoodContext);
+    
+        // const mood_context_form = formFields.moodContext;
+        // Send Update Query
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("POST", "http://localhost/Project/src/api/src/updateMoodEntryFromIDapi.php", false); // false for synchronous request
+        // API key
+        xmlHttp.setRequestHeader("X-API-KEY", api_key)
 
-//         // Send Delete (and log result)
-//         xmlHttp.send(data);
-//         console.log(xmlHttp.responseText);
+        // Form Data
+        var data;
+        data = new FormData();
+        data.append('MOOD_ENTRY_ID', mood_entry_id);
+        data.append('MOOD_CONTEXT_UPDATE', newMoodContext);
 
-//         // Redirect to Homepage
-//         window.location.href = "http://localhost/Project/src/client/";
-//     }
-// }
+        // Send Delete (and log result)
+        xmlHttp.send(data);
+        console.log(xmlHttp.responseText);
+
+        // Redirect to Homepage
+        window.location.href = "http://localhost/Project/src/client/";
+    }
+}
 
 function displayEditMoodEntryForm(moodEntry, api_key){
     // Unpack Mood Entry
@@ -62,10 +73,10 @@ function displayEditMoodEntryForm(moodEntry, api_key){
     var datetimeFormatted = getDateStringFromDateTime(datetime);
 
     var deleteConfirmString = "confirmDelete(event,\x22" + api_key + "\x22," + mood_entry_id + ")";
-    // var updateConfirmString = "confirmEdit(event,\x22" + api_key + "\x22," + mood_entry_id + ", "+ ")";
+    var editConfirmString = "confirmEdit(event,\x22" + api_key + "\x22," + mood_entry_id + ")";
 
     // create Form
-    var newForm = "<form method='POST'>"+
+    var newForm = "<form id='moodEditForm' method='POST'>"+
     // Mood Date (cant be changed)
     "<fieldset class='uk-fieldset'><legend class='uk-legend'>"+datetimeFormatted+"</legend>"+
     // Mood ID (hidden but used to pass to api)
@@ -77,7 +88,9 @@ function displayEditMoodEntryForm(moodEntry, api_key){
     "<div class='uk-margin'><input name = 'moodContext' class='uk-input' type='text' value='"+context+"'aria-label='Input'></div>"+
     // Update Buttons (including handle events for delete, edit)
     "<div class='uk-margin uk-grid-small uk-child-width-auto uk-grid'><p class='uk-margin'>"+
-        "<button id='updateMoodEntry' class='uk-button uk-button-default'>Update</button>"+
+        "<button id='updateMoodEntry' class='uk-button uk-button-default' "+
+        // Add handler for edit
+        "onclick="+editConfirmString+">Update</button>"+
         "<button id='deleteMoodEntry' class='uk-button uk-button-danger' "+
         // Add handler for delete
         "onclick="+deleteConfirmString+">Delete</button>"+
